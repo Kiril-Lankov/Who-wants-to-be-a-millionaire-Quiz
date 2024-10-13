a8657e4dcad25cf856616edfd19748d5ddfabf17
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-13 18:52:09 +0300

    App.jsx updated

---
+++ quiz/src/App.jsx
@@ -0,0 +1,19 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+     <div className="main">mine</div>
+     <div className="pyramid">
+      <ul className="moneyList">
+        <li moneyListItem>$ 400</li>
+        <li moneyListItem>$ 300</li>
+        <li moneyListItem>$ 200</li>
+        <li moneyListItem>$ 100</li>
+      </ul>
+     </div>
+    </div>
+  );
+}
+
+export default App;
