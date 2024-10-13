ae9aa48f331cbf96daa5c384be8116364f508578
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-13 20:37:25 +0300

    Update Quiz.jsx

--- quiz/src/Quiz.jsx
+++ quiz/src/Quiz.jsx
@@ -1,26 +1,37 @@
 import { useEffect, useState } from "react";
 
 export default function Quiz({ data, setTimeOut, questionNumber, setQuestionNumber }) {
-  const [question, setQuestion] = useState(null);
+    const [question, setQuestion] = useState(null);
+    const [questionMarker, setQuestionMarker] = useState(null);
+    const [className, setClassName] = useState("answer");
 
-  // Fetch question based on question number
-  useEffect(() => {
-    setQuestion(data[questionNumber - 1]);
-  }, [data, questionNumber]);
+    // Fetch question based on question number
+    useEffect(() => {
+        setQuestion(data[questionNumber - 1]);
+    }, [data, questionNumber]);
+    // Function to choose an answer of the question
+    const handleClick = (a) => {
+        setQuestionMarker(a);
+        setClassName("answer active");
+        // Check if the answer is correct
+        setTimeout(() => {
+            setClassName(a.correct ? "answer correct" : "answer wrong")
+        }, 3000);
+    }
 
-  return (
-    <div className="quiz">
-      
-      <div className="question">{question?.question}</div>
-      
-      
-      <div className="answers">
-        {question?.answers?.map((a, index) => (
-          <div key={index} className="answer">
-            {a.text}
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    return (
+        <div className="quiz">
+
+            <div className="question">{question?.question}</div>
+
+
+            <div className="answers">
+                {question?.answers?.map((a, index) => (
+                    <div key={index} className={questionMarker === a ? className : "answer"} onClick={() => handleClick(a)}>
+                        {a.text}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
