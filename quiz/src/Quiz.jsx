4d1fc3bab7611f2a371c0e47021922f8217130cd
Kiril-Lankov <k1r3t0@gmail.com>
2024-10-13 21:43:47 +0300

    Quiz.jsx updated sounds added

--- quiz/src/Quiz.jsx
+++ quiz/src/Quiz.jsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "./sounds/play.mp3";
+import correct from "./sounds/correct.mp3";
+import wrong from "./sounds/wrong.mp3";
 
 export default function Quiz({ data, setStop, questionNumber, setQuestionNumber }) {
     const [question, setQuestion] = useState(null);
     const [questionMarker, setQuestionMarker] = useState(null);
     const [className, setClassName] = useState("answer");
+    const [letsPlay] = useSound(play);
+    const [correctAnswer] = useSound(correct);
+    const [wrongAnswer] = useSound(wrong);
+
+    useEffect(()=>{
+        letsPlay();
+    }, [letsPlay]);
 
     // Fetch question based on question number
     useEffect(() => {
